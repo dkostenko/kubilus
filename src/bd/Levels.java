006a0f52bd99f2860e3b5fcb5a12c23bd6268763
dkostenko <kostenko.personal@gmail.com>
2013-03-10 17:18:33 +0400

    save

--- src/bd/Levels.java
+++ src/bd/Levels.java
@@ -20,6 +20,14 @@ public class Levels {
         switch(id){
             case 1: return getLevel1();
             case 2: return getLevel2();
+            case 3: return getLevel3();
+            case 4: return getLevel4();
+            case 5: return getLevel5();
+            case 6: return getLevel6();
+            case 7: return getLevel7();
+            case 8: return getLevel8();
+            case 9: return getLevel9();
+            case 10: return getLevel10();
         }
         
         return null;
@@ -42,6 +50,128 @@ public class Levels {
     }
 
     private static Level getLevel2(){
+        ArrayList<Block> blocks = new ArrayList<>();
+        Type[][] map = new Type[][]{{Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }, 
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.CLUBS, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.DIAMONDS, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.HEARTS, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.SPIDES, Type.WALL },
+                                   { Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }};
+        blocks.add(new Block(new Point(1, 1), 1, Type.CLUBS));
+        blocks.add(new Block(new Point(2, 1), 2, Type.DIAMONDS));
+        blocks.add(new Block(new Point(3, 1), 3, Type.HEARTS));
+        blocks.add(new Block(new Point(4, 1), 4, Type.SPIDES));
+        
+        return new Level(2, map, blocks, 10);
+    }
+    
+    
+    private static Level getLevel3(){
+        ArrayList<Block> blocks = new ArrayList<>();
+        Type[][] map = new Type[][]{{Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }, 
+                                   { Type.WALL, Type.CLUBS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.DIAMONDS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }};
+        blocks.add(new Block(new Point(2, 1), 1, Type.CLUBS));
+        blocks.add(new Block(new Point(2, 4), 2, Type.DIAMONDS));
+        
+        return new Level(2, map, blocks, 10);
+    }
+    
+    
+    private static Level getLevel4(){
+        ArrayList<Block> blocks = new ArrayList<>();
+        Type[][] map = new Type[][]{{Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }, 
+                                   { Type.WALL, Type.CLUBS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.DIAMONDS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }};
+        blocks.add(new Block(new Point(2, 1), 1, Type.CLUBS));
+        blocks.add(new Block(new Point(2, 4), 2, Type.DIAMONDS));
+        
+        return new Level(2, map, blocks, 10);
+    }
+    
+    
+    private static Level getLevel5(){
+        ArrayList<Block> blocks = new ArrayList<>();
+        Type[][] map = new Type[][]{{Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }, 
+                                   { Type.WALL, Type.CLUBS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.DIAMONDS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }};
+        blocks.add(new Block(new Point(2, 1), 1, Type.CLUBS));
+        blocks.add(new Block(new Point(2, 4), 2, Type.DIAMONDS));
+        
+        return new Level(2, map, blocks, 10);
+    }
+    
+    
+    private static Level getLevel6(){
+        ArrayList<Block> blocks = new ArrayList<>();
+        Type[][] map = new Type[][]{{Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }, 
+                                   { Type.WALL, Type.CLUBS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.DIAMONDS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }};
+        blocks.add(new Block(new Point(2, 1), 1, Type.CLUBS));
+        blocks.add(new Block(new Point(2, 4), 2, Type.DIAMONDS));
+        
+        return new Level(2, map, blocks, 10);
+    }
+    
+    
+    private static Level getLevel7(){
+        ArrayList<Block> blocks = new ArrayList<>();
+        Type[][] map = new Type[][]{{Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }, 
+                                   { Type.WALL, Type.CLUBS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.DIAMONDS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }};
+        blocks.add(new Block(new Point(2, 1), 1, Type.CLUBS));
+        blocks.add(new Block(new Point(2, 4), 2, Type.DIAMONDS));
+        
+        return new Level(2, map, blocks, 10);
+    }
+    
+    
+    private static Level getLevel8(){
+        ArrayList<Block> blocks = new ArrayList<>();
+        Type[][] map = new Type[][]{{Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }, 
+                                   { Type.WALL, Type.CLUBS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.DIAMONDS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }};
+        blocks.add(new Block(new Point(2, 1), 1, Type.CLUBS));
+        blocks.add(new Block(new Point(2, 4), 2, Type.DIAMONDS));
+        
+        return new Level(2, map, blocks, 10);
+    }
+    
+    
+    private static Level getLevel9(){
+        ArrayList<Block> blocks = new ArrayList<>();
+        Type[][] map = new Type[][]{{Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }, 
+                                   { Type.WALL, Type.CLUBS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.NONE, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.DIAMONDS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
+                                   { Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }};
+        blocks.add(new Block(new Point(2, 1), 1, Type.CLUBS));
+        blocks.add(new Block(new Point(2, 4), 2, Type.DIAMONDS));
+        
+        return new Level(2, map, blocks, 10);
+    }
+    
+    
+    private static Level getLevel10(){
         ArrayList<Block> blocks = new ArrayList<>();
         Type[][] map = new Type[][]{{Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL, Type.WALL }, 
                                    { Type.WALL, Type.CLUBS, Type.NONE, Type.NONE, Type.NONE, Type.WALL },
